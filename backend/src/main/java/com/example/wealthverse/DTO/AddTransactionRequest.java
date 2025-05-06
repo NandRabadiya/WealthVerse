79fd59ecc323f2387d115d051187ba45c2dfe172
VenuVirparia <venupatel004@gmail.com>
2025-05-06 20:11:47 +0530

    Revised AddTransactionRequest DTO

--- backend/src/main/java/com/example/wealthverse/DTO/AddTransactionRequest.java
+++ backend/src/main/java/com/example/wealthverse/DTO/AddTransactionRequest.java
@@ -2,7 +2,9 @@ package com.example.wealthverse.DTO;
 
 import com.example.wealthverse.Enums.PaymentMode;
 import com.example.wealthverse.Enums.TransactionType;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -18,4 +20,7 @@ public class AddTransactionRequest {
     private String merchantName;
 
     private TransactionType transactionType;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime createdAt;
 }
