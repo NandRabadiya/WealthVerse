a496e812423bb65cf199bf681c6c142b2588d5e5
Nand Rabadiya <nandrabadiya2003@gmail.com>
2025-05-06 10:09:41 +0530

    Backend Initialise

---
+++ backend/src/main/java/com/example/wealthverse/WealthverseApplication.java
@@ -0,0 +1,13 @@
+package com.example.wealthverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WealthverseApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/wealthverse/WealthverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wealthverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WealthverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
