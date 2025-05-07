3532211afa55e6f07925d591493fa47275812c44
Nand Rabadiya <nandrabadiya2003@gmail.com>
2025-05-07 16:35:01 +0530

    Conflit resolved and CategoryController updated.

--- backend/src/main/java/com/example/wealthverse/Controller/CategoryController.java
+++ backend/src/main/java/com/example/wealthverse/Controller/CategoryController.java
@@ -4,6 +4,7 @@ import com.example.wealthverse.Model.Category;
 import com.example.wealthverse.DTO.EmissionCalculationRequest;
 import com.example.wealthverse.Service.CategoryService;
 import com.example.wealthverse.Service.Impl.CategoryServiceImpl;
+import com.example.wealthverse.Service.JWTService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,8 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    @Autowired
+    private JWTService jwtService;
 
     @PostMapping("/calculate")
     public BigDecimal calculateEmission(@RequestBody EmissionCalculationRequest request) {
@@ -34,10 +37,14 @@ public class CategoryController {
 
         categoryService.addCustomCategory(category, authHeader);
         return ResponseEntity.status(HttpStatus.CREATED).body("Category created successfully");
+    }
 
-    @GetMapping("/user/{userId}")
-    public List<Category> getCategoriesByUserId(@PathVariable Long userId) {
-        return categoryServiceImpl.getAllCategoriesByUserId(userId);
+    @GetMapping("/user")
+    public List<Category> getCategoriesByUserId(@RequestHeader("Authorization") String token) {
+
+        Long userId= jwtService.getUserIdFromToken(token);
+
+        return categoryService.getAllCategoriesByUserId(userId);
     }
 
 }
